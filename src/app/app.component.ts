98d2ea1118ab22df03701cef5aa8f38715b786d7
camilufer <camilu.fer21@gmail.com>
2018-02-26 18:12:30 -0300

    primera clase angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Cadaver Exquisito';
+  poemas = [
+  true,
+  true
+  ];
+
+  onPoemMostrar(input){
+  this.poemas[input.id] = input.mostrar;
+  }
+}
